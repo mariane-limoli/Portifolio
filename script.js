bde262a44c063cdca58a2872734865761725bace
mariane-limoli <marianelimoli@gmail.com>
2023-05-24 15:05:45 -0300

    portofólio | Mariane

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+// scroll sections
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll =  ( )=> {
+sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+        // Ativar cor para navegação
+
+        navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+        })
+    }
+});
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove icone toggle e navbar quando clickar nos links da navbar(scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
